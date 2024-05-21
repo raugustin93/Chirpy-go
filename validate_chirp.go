75077707d053eeee623f583bd40c25854a2faa88
Rudson <rudson.augustin@yahoo.com>
2024-05-21 19:57:08 -0400

    profanity test

--- validate_chirp.go
+++ validate_chirp.go
@@ -2,55 +2,56 @@ package main
 
 import (
 	"encoding/json"
-	"log"
+	"errors"
 	"net/http"
+	"strings"
 )
 
-func validate_chirp(w http.ResponseWriter, r *http.Request) {
-	type parameters struct {
-		Body string `json:"body"`
-	}
+type parameters struct {
+	Body string `json:"body"`
+}
 
-	type errResponse struct {
-		Error string `json:"error"`
+func validate_chirp(w http.ResponseWriter, r *http.Request) {
+	type returnVals struct {
+		CleanedBody string `json:"cleaned_body"`
 	}
 
 	decoder := json.NewDecoder(r.Body)
 	params := parameters{}
 	err := decoder.Decode(&params)
 	if err != nil {
-		log.Printf("Error decoding parameters: %s", err)
-		w.WriteHeader(500)
+		respondWithError(w, http.StatusInternalServerError, "Couldn't decode parameters")
 		return
 	}
 
-	w.Header().Set("Content-Type", "application/json")
+	const maxChirpLength = 140
+	if len(params.Body) > maxChirpLength {
+		respondWithError(w, http.StatusBadRequest, "Chirp is too long")
+		return
+	}
 
-	if len(params.Body) > 140 {
-		error := errResponse{Error: "Chirp is too long"}
-		dat, err := json.Marshal(error)
-		if err != nil {
-			log.Printf("Error marshalling JSON: %s", err)
-			w.WriteHeader(500)
-			return
+	cleaned_body := params.Body
+
+	hasProfanity := profanity(params)
+	if hasProfanity != nil {
+		cleaned_body = hasProfanity.Error()
+	}
+
+	respondWithJSON(w, http.StatusOK, returnVals{CleanedBody: cleaned_body})
+}
+
+func profanity(params parameters) error {
+	body := strings.Split(params.Body, " ")
+	badWords := []string{"kerfuffle", "sharbert", "fornax"}
+	for _, badWord := range badWords {
+		for i, word := range body {
+			if strings.ToLower(word) == badWord {
+				body[i] = "****"
+			}
 		}
-
-		w.WriteHeader(400)
-		w.Write(dat)
-		return
 	}
-
-	respBody := struct {
-		Valid bool `json:"valid"`
-	}{Valid: true}
-
-	dat, err := json.Marshal(respBody)
-	if err != nil {
-		log.Printf("Error marshalling JSON: %s", err)
-		w.WriteHeader(500)
-		return
+	if params.Body != strings.Join(body, " ") {
+		return errors.New(strings.Join(body, " "))
 	}
-
-	w.WriteHeader(200)
-	w.Write(dat)
+	return nil
 }
